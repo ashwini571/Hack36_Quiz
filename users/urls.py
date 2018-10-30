6779c22318eeeba3aa1f0a57f4ee98ec4b979a8d
unknown <ojha.ashwini.1998@gmail.com>
2018-10-30 16:42:10 +0530

    Project Initialized

---
+++ users/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('login', views.login, name='login'),
+    path('signup', views.signup, name='signup'),
+    path('logout', views.logout, name='logout'),
+    path('profile', views.profile, name ='profile'),
+    path('dashboard', views.dash, name='dashboard'),
+    path('edit_profile', views.update_profile, name='edit_profile'),
+    path('edit_profile', views.update_profile, name='edit_profile'),
+    path('profile/<slug:username>', views.public_profile, name='profile'),
+
+
+]
---
+++ users/views.py
@@ -0,0 +1,115 @@
+from django.shortcuts import render, redirect ,get_object_or_404
+from django.contrib.auth.models import User
+from django.contrib import auth, messages
+from django.db import transaction
+from django.contrib.auth.decorators import login_required
+from .forms import UserForm, ProfileForm
+from quiz.models import Quiz,Score
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from .models import Profile
+import os
+
+
+def signup(request):
+    if request.method == 'POST':
+        #USER wants to sign up
+        if request.POST['password1'] == request.POST['password2']:
+            #Entered passwords are identical
+            try:
+                # #check if user already exists in database if yes raise error
+
+                user = User.objects.get(username = request.POST['username'])
+                return render(request, 'signup.html', {'error': 'Username is already taken!'})
+                #
+
+            except:
+               try:
+                usermail = User.objects.get(email = request.POST['email'])
+                return render(request, 'signup.html', {'error': 'Email is already taken!'})
+               except User.DoesNotExist:
+                   # username is available
+                   user = User.objects.create_user(username=request.POST['username'],
+                                                   password=request.POST['password1'], email=request.POST['email'], )
+                   auth.login(request, user)
+                   return redirect('edit_profile')
+        else:
+            return render(request, 'signup.html', {'error2': 'Passwords do not match!'})
+
+    else:
+        #If request is get
+        if request.user.is_authenticated:
+            return render(request,'dashboard.html')
+        else:
+            return render(request, 'signup.html')
+
+@login_required(login_url = '/accounts/login')
+@transaction.atomic
+def update_profile(request):
+    if request.method == 'POST':
+        user_form = UserForm(request.POST, instance=request.user)
+        profile_form = ProfileForm(request.POST, request.FILES, instance=request.user.profile)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(request, 'Your profile was successfully updated!')
+            return redirect('dashboard')
+        else:
+            messages.error(request, 'Please correct the error below.')
+    else:
+        user_form = UserForm(instance=request.user)
+        profile_form = ProfileForm(instance=request.user.profile)
+    return render(request, 'profile.html', {
+        'user_form': user_form,
+        'profile_form': profile_form
+    })
+
+@login_required(login_url = '/accounts/login')
+def profile(request):
+    data = Score.objects.filter(applicant=request.user)
+    return render(request, 'userprofile.html', {'scores': data})
+
+def login(request):
+    if request.method == 'POST':
+        user = auth.authenticate(username = request.POST['username'], password= request.POST['password'])
+        if user is not None:
+            auth.login(request, user)
+            item=Quiz.objects.all().order_by('name')
+            paginator = Paginator(item, 5)  # Show 1o quizzes per page
+            page = request.GET.get('page', 1)
+            try:
+                item = paginator.get_page(page)
+            except PageNotAnInteger:
+                item = paginator.get_page(1)
+            except EmptyPage:
+                item = paginator.get_page(paginator.num_pages)
+
+            return render(request,'dashboard.html',{'quiz_object':item})
+        else:
+            return render(request, 'base.html', {'error': 'Invalid Credentials! Please enter correct username and password.'})
+    else:
+        return redirect('dashboard')
+
+def logout(request):
+    if request.method == 'POST':
+        auth.logout(request)
+        return render(request,'home.html')
+
+@login_required(login_url = '/accounts/login')
+def dash(request):
+    item = Quiz.objects.all().order_by('name')
+    paginator = Paginator(item, 5)  # Show 1o quizzes per page
+    page = request.GET.get('page', 1)
+    try:
+        item = paginator.get_page(page)
+    except PageNotAnInteger:
+        item = paginator.get_page(1)
+    except EmptyPage:
+        item = paginator.get_page(paginator.num_pages)
+
+    return render(request, 'dashboard.html', {'quiz_object': item})
+def public_profile(request, username):
+    user = get_object_or_404(User, username = username)
+    data = Score.objects.filter(applicant=user)
+    return render(request, 'publicprofile.html', {'person': user, 'scores': data})
+    
+    
